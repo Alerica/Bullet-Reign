cb2991541ad60f633e7f812cff841c18ef3b34a8
Alexander Ricky <146740660+Alerica@users.noreply.github.com>
2025-03-21 15:54:48 +0700

    Gun Rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,30 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Rigidbody2D rb2d;
-
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform gun;
 
     [Header("Attributes")]
     [SerializeField] private float playerSpeed = 5f;
 
     Vector2 movement = Vector2.zero;
+    Vector2 mousePosition = Vector2.zero;
     
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         Debug.Log("X: " + movement.x + " Y: " + movement.y);
+
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
     }
 
     void FixedUpdate()
     {
-        rb2d.MovePosition(rb2d.position + movement * playerSpeed * Time.fixedDeltaTime);   
+        rb2d.MovePosition(rb2d.position + movement * playerSpeed * Time.fixedDeltaTime);  
+
+        Vector2 lookDirection = mousePosition - rb2d.position;
+        float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg - 90f;
+        gun.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
